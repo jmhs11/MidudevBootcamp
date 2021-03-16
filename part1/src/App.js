c419b680f9c05f71b9ea65cc043d1407ba4f3055
jmhs11 <josemariahs@gmail.com>
2021-03-16 20:36:50 +0100

    First Commit

---
+++ part1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Mensaje msg='Esto es el Componente Mensaje desde App'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/src/Mensaje.js
@@ -0,0 +1,7 @@
+const Mensaje = (props) => {
+    return (
+        <p>{props.msg}</p>
+    )
+}
+
+export default Mensaje
