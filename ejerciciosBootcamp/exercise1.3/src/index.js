b9b5eaa9a4be0115adc9689f59940e3505594ac9
jmhs11 <josemariahs@gmail.com>
2021-03-20 21:28:56 +0100

    Finished ex1.3

---
+++ ejerciciosBootcamp/exercise1.3/src/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/**
+ * Avancemos para usar objetos en nuestra aplicación. 
+ * Modifique las definiciones de las variables del componente 
+ * App de la siguiente manera y también refactorice la aplicación 
+ * para que siga funcionando:
+ */
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ part1, part2, part3 }) => {
+  return (
+    <div>
+      <Part part={part1.name} exercise={part1.exercises} />
+      <Part part={part2.name} exercise={part2.exercises} />
+      <Part part={part3.name} exercise={part3.exercises} />
+    </div>
+  )
+}
+
+const Part = ({ part, exercise }) => {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
+  )
+}
+
+const Total = ({ exercises }) => {
+  return (
+    <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
